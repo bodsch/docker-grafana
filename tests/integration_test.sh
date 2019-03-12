785731478d4e21d1e1310688578b57a8fb24f8db
Bodo Schulz <bodo@boone-schulz.de>
2019-03-12 19:05:05 +0100

    fix test

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -8,9 +8,11 @@ CURL=$(which curl 2> /dev/null)
 NC=$(which nc 2> /dev/null)
 NC_OPTS="-z"
 
+API_TOKEN_FILE="/tmp/grafana.test"
+
 wait_for_grafana() {
 
-  echo "wait for grafana"
+  echo -e "\nwait for grafana"
   RETRY=35
   until [[ ${RETRY} -le 0 ]]
   do
@@ -34,37 +36,92 @@ wait_for_grafana() {
 
 create_token() {
 
-  curl_opts="--silent --user admin:admin"
-  out_file="/tmp/grafana.test"
+  echo -e "\ncreate API token"
 
-  code=$(curl \
+  api_key="Spec Test"
+
+  curl_opts="--silent --user admin:admin"
+
+  data=$(curl \
     ${curl_opts} \
-    --request POST \
     --header "Content-Type: application/json" \
-    --write-out '%{http_code}\n' \
-    --output ${out_file} \
-    --data '{"name":"Spec Test", "role": "Admin"}' \
     http://localhost:${GRAFANA_PORT}/api/auth/keys)
 
   result=${?}
 
-  echo "code: ${code}"
+  existing_api_key=$(echo "${data}" | jq --raw-output .[].name)
 
-  if [[ ${result} -eq 0 ]] && [[ ${code} = 200 ]]
+  if [[ -n ${existing_api_key} ]] && [[ -f ${API_TOKEN_FILE} ]]
   then
-    echo "token request are successfull"
+    echo "  reuse token"
 
-    TOKEN=$(jq --raw-output .key ${out_file})
-
-    export TOKEN
+    TOKEN=$(jq --raw-output .key ${API_TOKEN_FILE})
+    API_NAME=$(jq --raw-output .name ${API_TOKEN_FILE})
   else
-    echo ${code}
-    echo "api request failed"
+
+    code=$(curl \
+      ${curl_opts} \
+      --request POST \
+      --header "Content-Type: application/json" \
+      --write-out '%{http_code}\n' \
+      --output ${API_TOKEN_FILE} \
+      --data "{\"name\":\"${api_key}\", \"role\": \"Admin\"}" \
+      http://localhost:${GRAFANA_PORT}/api/auth/keys)
+
+    result=${?}
+
+    if [[ ${result} -eq 0 ]] && [[ ${code} = 200 ]]
+    then
+      echo "  token request are successfull"
+
+      TOKEN=$(jq --raw-output .key ${API_TOKEN_FILE})
+
+      export TOKEN
+    else
+      echo ${code}
+      echo "  token request failed"
+
+      exit 1
+    fi
+
   fi
 }
 
 
+remove_token() {
+
+  curl_opts="--silent "
+
+  HEADERS=( "content-type: application/json;charset=UTF-8" )
+
+  if [[ -z "${TOKEN}" ]]
+  then
+    curl_opts="${curl_opts} --user admin:admin"
+  else
+    HEADERS+=( "Authorization: Bearer ${TOKEN}" )
+  fi
+
+  for((i=0; i<${#HEADERS[@]}; i++)); do
+    parameters+=("--header" "${HEADERS[$i]}");
+  done
+
+  cat ${API_TOKEN_FILE}
+
+  curl \
+    ${curl_opts} \
+    "${parameters[@]}" \
+    --request DELETE \
+    --header "Content-Type: application/json" \
+    http://localhost:${GRAFANA_PORT}/api/auth/keys/1
+
+  rm -f ${API_TOKEN_FILE}
+
+}
+
+
 api_request() {
+
+  echo ""
 
   curl_opts="--silent "
 
@@ -103,14 +160,14 @@ api_request() {
 
   if [[ $? -eq 0 ]]
   then
-    echo "api request are successfull"
+    echo "  api request are successfull"
     echo "${code}" | jq --raw-output ".message"
   else
     echo ${code}
-    echo "api request failed"
+    echo "  api request failed"
   fi
 
-  echo "restore organistation to '${name}'"
+  echo -e "\nrestore organistation to '${name}'"
   code=$(curl \
     ${curl_opts} \
     "${parameters[@]}" \
@@ -119,13 +176,12 @@ api_request() {
     --data-binary "{\"name\":\"${name}\"}" \
     http://localhost:${GRAFANA_PORT}/api/org)
 
-
-  echo "number of datasources"
+  echo -e "\nnumber of datasources"
   code=$(curl \
     ${curl_opts} \
     "${parameters[@]}" \
-    --header 'Content-Type: application/json;charset=UTF-8' \
     --request GET \
+    --header 'Content-Type: application/json;charset=UTF-8' \
     http://localhost:${GRAFANA_PORT}/api/datasources)
 
   echo "  $(echo "${code}"  | jq --raw-output '.[].name' | wc -l)"
@@ -142,7 +198,6 @@ api_request() {
     data=$(file \
       --mime \
       qa-test.png)
-    # qa-test.png: image/png; charset=binary
 
     if [[ $? -eq 0 ]]
     then
@@ -159,8 +214,7 @@ api_request() {
 
 inspect() {
 
-  echo ""
-  echo "inspect needed containers"
+  echo -e "\ninspect needed containers"
   for d in $(docker ps | tail -n +2 | awk  '{print($1)}')
   do
     c=$(docker inspect --format '{{with .State}} {{$.Name}} has pid {{.Pid}} {{end}}' ${d})
@@ -177,9 +231,11 @@ if [[ ${running_containers} -eq 4 ]] || [[ ${running_containers} -gt 4 ]]
 then
   inspect
 
-  #wait_for_grafana
+  wait_for_grafana
   create_token
   api_request
+
+  remove_token
 
   exit 0
 else
